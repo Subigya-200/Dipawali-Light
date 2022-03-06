ece02cb1954553f96f1287f6a7c6b24ee511e851
SUBIGYA SUBEDI <100819803+Subigya-200@users.noreply.github.com>
2022-03-06 17:02:50 +0545

    Add files via upload

---
+++ Dipawali Light Using C/happyDipawali.c
@@ -0,0 +1,61 @@
+//Dipawali Light using C
+
+#include <stdio.h>
+int main()
+{
+    //For Aago(Fire)
+    int i, j, n, space;
+    n = 3;
+    printf("\n\n");
+    for (i = 1; i <= n; i++)
+    {
+        for (space = 1; space <= ((3 * n) + 1 - i); space++)
+        {
+            printf(" ");
+        }
+
+        for (j = 1; j <= (2 * i) - 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    printf("       *****");
+    printf("\n");
+
+    for (i = 1; i <= n; i++)
+    {
+        for (space = 1; space <= (8 + i - 1); space++)
+        {
+            printf(" ");
+        }
+        for (j = 3; j >= (2 * i) - 1; j--)
+        {
+            printf("*");
+
+        }
+        printf("\n");
+    }
+
+    //For Diyo
+
+    n = 4;
+    for (i = 1; i <= n; i++)
+    {
+        for (space = 1; space <= (i - 1); space++)
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= (20 - (2 * i) + 1); j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    printf("\n");
+    printf("***#### Happy Dipawali ####***");
+    printf("\n\n");
+
+    return 0;
+}
